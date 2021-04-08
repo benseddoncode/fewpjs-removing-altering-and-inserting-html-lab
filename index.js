bcd1ae02874acbda27840df891206f04ba9a0799
benseddoncode <ben.seddon@aussiebb.com.au>
2021-04-08 17:14:25 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let removeMain = document.getElementById('main');
+
+removeMain.remove();
+
+
+
+let newHeader = document.createElement('h1');
+newHeader.id='victory';
+newHeader.innerHTML ="BENS is the champion"
